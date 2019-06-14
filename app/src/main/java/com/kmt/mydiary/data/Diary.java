6132653ba3a1d168b80b1f5ab4c45d3740c48029
KidusMT <kidusmamuye@gmail.com>
2019-06-14 23:36:50 +0300

    created diary entity class

---
+++ app/src/main/java/com/kmt/mydiary/data/Diary.java
@@ -0,0 +1,53 @@
+package com.kmt.mydiary.data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "diary")
+public class Diary {
+
+    @ColumnInfo(name = "title")
+    String title;
+
+    @ColumnInfo(name = "date")
+    String date;
+
+    @ColumnInfo(name = "body")
+    String body;
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}

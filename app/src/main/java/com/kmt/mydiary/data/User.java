cea5299ba4f2b16be968f59fe05da3758db3e7c8
KidusMT <kidusmamuye@gmail.com>
2019-06-14 23:39:48 +0300

    created the User Database model

---
+++ app/src/main/java/com/kmt/mydiary/data/User.java
@@ -0,0 +1,64 @@
+package com.kmt.mydiary.data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "user")
+public class User {
+
+    @ColumnInfo(name = "name")
+    String name;
+
+    @ColumnInfo(name = "password")
+    String password;
+
+    @ColumnInfo(name = "username")
+    String username;
+
+    @ColumnInfo(name = "email")
+    String email;
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

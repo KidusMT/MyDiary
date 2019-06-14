64bf9e582ab74e851ce7b4089f9389f2e9c1e5e8
KidusMT <kidusmamuye@gmail.com>
2019-06-14 23:42:04 +0300

    re-arranged the project files and created the App instantiating class for the project

---
+++ app/src/main/java/com/kmt/mydiary/App.java
@@ -0,0 +1,6 @@
+package com.kmt.mydiary;
+
+import android.app.Application;
+
+public class App extends Application {
+}
--- app/src/main/java/com/kmt/mydiary/ui/MainActivity.java
+++ app/src/main/java/com/kmt/mydiary/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.kmt.mydiary;
+package com.kmt.mydiary.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.kmt.mydiary.R;
 
 public class MainActivity extends AppCompatActivity {
 
